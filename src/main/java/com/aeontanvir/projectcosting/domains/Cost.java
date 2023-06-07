1196101cf5cbd677e297ac2fd3ebafc76735656f
Tanvir Rahman Khan <aeontanvir@gmail.com>
2023-06-06 23:13:02 -0500

    Build cost and project relationship

--- src/main/java/com/aeontanvir/projectcosting/domains/Cost.java
+++ src/main/java/com/aeontanvir/projectcosting/domains/Cost.java
@@ -1,15 +1,27 @@
 package com.aeontanvir.projectcosting.domains;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "_cost")
 public class Cost {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String title;
     private String description;
     private double amount;
+    @ManyToOne
+    @JoinColumn(name = "project_id")
+    private Project project;
+
+
 }
--- src/main/java/com/aeontanvir/projectcosting/domains/Project.java
+++ src/main/java/com/aeontanvir/projectcosting/domains/Project.java
@@ -1,15 +1,24 @@
 package com.aeontanvir.projectcosting.domains;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Porject {
+@Entity
+@Table(name = "_project")
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String title;
     private String description;
     private double budget;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "project")
+    private List<Cost> costs;
 }
